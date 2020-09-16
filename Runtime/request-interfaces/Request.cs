23c562c53e3ede7b83c8c6351f8d3c9dfe622880
larry kirschner <larrykirschner@gmail.com>
2020-09-16 10:16:46 -0700

    adds loggingName prop and some ext functions
     * feature: adds loggingName prop to help identify requests in logs
     * feature: adds ext functions IsQueuedOrInProgress,
    CanExecuteFromStatus, etc

--- Runtime/request-interfaces/Request.cs
+++ Runtime/request-interfaces/Request.cs
@@ -27,6 +27,11 @@ namespace BeatThat.Requests
         /// </summary>
         void Execute(Action callback = null, bool callbackOnCancelled = false);
 
+        ///<summary>
+        /// often a web uri, but can be anything that helps identify the request
+        ///</summary>
+        string loggingName { get; }
+
         /// <summary>
         /// Signal to log debug info for a specific request. 
         /// Ususally more useful than a global flag, because there can be so many requests running at a time, 
@@ -51,23 +56,66 @@ namespace BeatThat.Requests
         T item { get; }
     }
 
-    namespace Ext
+    public static class RequestExt
     {
-        public static class RequestExt
+        public static bool CanExecuteFromCurrentStatus(this Request r)
         {
-            public static T GetResult<T>(this Request r)
+            if (r == null)
             {
-                if (r == null)
-                {
-                    return default(T);
-                }
-
-                var hasItem = r as ItemRequest;
-                return hasItem != null ? (T)hasItem.GetItem() : default(T);
-
+                return false;
             }
+            return (
+                r.status == RequestStatus.NONE
+                || r.status == RequestStatus.QUEUED);
+        }
+
+        /// <summary>
+        /// Utility for cases where a service stores the 'active' request,
+        /// e.g. to prevent duplicate concurrent requests.
+        /// If the passed request is the same ref as the passed ref, then nulls the ref
+        /// If not, clears the request (this is why it's a ref arg)
+        /// </summary>
+        public static bool ClearIfMatches<T>(this Request r, ref T rRef) where T : class
+        {
+            if (r == null)
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(r, rRef))
+            {
+                rRef = null;
+                return true;
+            }
+            return false;
+        }
+
+
+        public static bool IsDoneOrCancelled(this Request r)
+        {
+            if (r == null)
+            {
+                return false;
+            }
+            return (
+                r.status == RequestStatus.DONE
+                || r.status == RequestStatus.CANCELLED);
+        }
+
+        public static bool IsQueuedOrInProgress(this Request r)
+        {
+            return r.status == RequestStatus.QUEUED || r.status == RequestStatus.IN_PROGRESS;
+        }
+
+        public static T GetResult<T>(this Request r)
+        {
+            if (r == null)
+            {
+                return default(T);
+            }
+            var hasItem = r as ItemRequest;
+            return hasItem != null ? (T)hasItem.GetItem() : default(T);
+
         }
     }
-
 }
 
